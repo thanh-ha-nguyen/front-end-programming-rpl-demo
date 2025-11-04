e67796dd1959d3dbbaa155d1db9d2125d71bde50
Thanh Ha Nguyen <thanh-ha.nguyen@myy.haaga-helia.fi>
2025-11-05 00:57:31 +0700

    Install and setup Material UI.

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import CssBaseline from '@mui/material/CssBaseline'
+import '@fontsource/roboto/300.css'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/500.css'
+import '@fontsource/roboto/700.css'
 import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <CssBaseline />
     <App />
   </StrictMode>,
 )
