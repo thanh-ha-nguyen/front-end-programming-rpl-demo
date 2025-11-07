33007a64ceb31e72f758b1c7165cadba769d4fe0
Thanh Ha Nguyen <thanh-ha.nguyen@myy.haaga-helia.fi>
2025-11-07 17:33:14 +0700

    Add definition for data models.

---
+++ src/models.d.ts
@@ -0,0 +1,25 @@
+interface Entity {
+  id: number;
+}
+
+interface Customer {
+  firstname: string;
+  lastname: string;
+  streetaddress: string;
+  postcode: string;
+  city: string;
+  email: string;
+  phone: string;
+}
+
+interface CustomerEntity extends Entity, Customer {}
+
+interface Training {
+  date: Date;
+  duration: number;
+  activity: string;
+}
+
+interface TrainingEntity extends Entity, Training {
+  customer: CustomerEntity;
+}
